790c779bd59a0e6e7e4b6b1e6a5c832d325071b2
JinGrin <148150574+JinGrin@users.noreply.github.com>
2023-10-22 11:00:21 +0300

    update Lesson1/VariablesTheme.java

--- Lesson1/VariablesTheme.java
+++ Lesson1/VariablesTheme.java
@@ -2,5 +2,11 @@
 public class VariablesTheme {	
     public static void main(String[] args) {
         System.out.println("Wr");
+        // Miscellaneous Variables (Разные переменные)
+        int number = 5;
+        int sumOfNumbers = 63;
+        int productOfNumbers = 678;
+        int maximumNumber = 58;
+        System.out.println(number);
     }
 }
